9eab4a608733e3bf27314e15d351474b342fa803
Matheus Felipe Marques <41908544+Matheus2212@users.noreply.github.com>
2021-01-20 13:57:26 -0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head>
-
-    </head>
-    <body style="background:red">
-        <img src="gera_foto.php?imagem=minion.png&perfil=nome_perfil" alt="">
-        <!--<img src="gera_foto_bkp.php?imagem=fundo.jpg&perfil=nome_perfil" alt="">-->
-    </body>
-</html>
